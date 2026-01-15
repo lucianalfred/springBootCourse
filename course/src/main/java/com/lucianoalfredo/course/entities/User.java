15c79948d7cfbefdab53034fda4d65ca7d4679e7
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-15 16:30:59 +0100

    feat: h2 and jakarta persistence

--- course/src/main/java/com/lucianoalfredo/course/entities/User.java
+++ course/src/main/java/com/lucianoalfredo/course/entities/User.java
@@ -5,12 +5,24 @@ import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String email;
 	private String phone;
