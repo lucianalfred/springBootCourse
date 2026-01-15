b8f7f21821b055244853ea3af412d17d3a46d102
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-15 15:31:09 +0100

    feat: user class and resource

--- course/src/main/java/com/lucianoalfredo/course/entities/User.java
+++ course/src/main/java/com/lucianoalfredo/course/entities/User.java
@@ -1,5 +1,80 @@
 package com.lucianoalfredo.course.entities;
 
-public class User {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class User implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String email;
+	private String phone;
+	private String password;
+	
+	public User() {
+	}
+
+	public User(Long id, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	protected Long getId() {
+		return id;
+	}
+
+	protected void setId(Long id) {
+		this.id = id;
+	}
+
+	protected String getEmail() {
+		return email;
+	}
+
+	protected void setEmail(String email) {
+		this.email = email;
+	}
+
+	protected String getPhone() {
+		return phone;
+	}
+
+	protected void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	protected String getPassword() {
+		return password;
+	}
+
+	protected void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+
+
+	
+	
 }
