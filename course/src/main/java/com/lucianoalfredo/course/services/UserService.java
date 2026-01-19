fd3ea9acab498a4e8e52830d671a1376f1b7fdcc
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-19 15:31:03 +0100

    feat: findById service

--- course/src/main/java/com/lucianoalfredo/course/services/UserService.java
+++ course/src/main/java/com/lucianoalfredo/course/services/UserService.java
@@ -1,9 +1,10 @@
 package com.lucianoalfredo.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
 import org.springframework.stereotype.Service;
 
 import com.lucianoalfredo.course.repositories.UserRepository;
@@ -21,4 +22,9 @@ public class UserService {
 		return repository.findAll();
 	}
  	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		
+		return obj.get();
+	}
 }
