f9399232098b2abb4c2cceea5628419c558c9bd7
Luciano Alfredo <lucianalfred60@gmail.com>
2026-01-19 15:36:16 +0100

    feat: findById resource

--- course/src/main/java/com/lucianoalfredo/course/resources/UserResource.java
+++ course/src/main/java/com/lucianoalfredo/course/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.lucianoalfredo.course.entities.User;
 import com.lucianoalfredo.course.services.UserService;
@@ -24,6 +25,12 @@ public class UserResource {
 		List<User> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/id")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
